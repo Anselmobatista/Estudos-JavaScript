862b00e1c8a236f236455053852eb0c3373b605a
Anselmo Batista <67919142+Anselmobatista@users.noreply.github.com>
2020-07-09 11:34:50 -0300

    Inicio estudos do while

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,25 @@
+/*teste lógico no inicio*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 10)
+
+
+
+/* teste lógico no inicio
+var c = 1
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+    */
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/

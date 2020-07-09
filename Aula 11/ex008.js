399983f9b00d529d0ef7aa05dd52c36b4224a06e
Anselmo Batista <67919142+Anselmobatista@users.noreply.github.com>
2020-07-09 11:33:09 -0300

    Sistema de multas

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //CONDIÇÃO SIMPLÊS
+    console.log(`Você ultrapassou o limite permitido. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
